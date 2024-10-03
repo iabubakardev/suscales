99c62b7b9c2e6fa1bbe37312524bc2a21401b1f5
Abu Bakar <iabubakardev@gmail.com>
2024-10-04 00:29:27 +0500

    fix

--- assets/js/custom_style.js
+++ assets/js/custom_style.js
@@ -1,5 +1,5 @@
 const typed = new Typed("#typed", {
-	strings: ["Scalability", "Flexibility", "Elasticity", "Versatility"],
+	strings: ["sustainable <span class='war-yellow'>scalability</span>"],
 	typeSpeed: 50,
 	backSpeed: 50,
 	backDelay: 2000,
